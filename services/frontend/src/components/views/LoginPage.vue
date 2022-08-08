008219998a6db6da30f2750b45f2096c388c38f9
Shyngys Karishev <karishev@karishev.local>
2022-08-08 21:57:49 +0600

    feat: login page added

---
+++ services/frontend/src/components/views/LoginPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="login-page">
+    <div>
+      Login
+      <input />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+};
+</script>
+
+<style scoped>
+.login-page {
+  padding: 6rem 2rem;
+}
+</style>
