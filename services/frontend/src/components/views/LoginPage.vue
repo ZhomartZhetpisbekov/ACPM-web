f843dd0ff496f154084f20eb7a514b497bae32db
Shyngys Karishev <karishev@karishev.local>
2022-08-09 02:11:16 +0600

    style: login page changed

--- services/frontend/src/components/views/LoginPage.vue
+++ services/frontend/src/components/views/LoginPage.vue
@@ -1,13 +1,22 @@
 <template>
   <section class="login-page">
     <form>
-      <h3>Login</h3>
+      <h3>Login to Your Account</h3>
 
       <label for="username">Username</label>
       <input type="text" placeholder="Email or Phone" id="username" />
 
       <label for="password">Password</label>
       <input type="password" placeholder="Password" id="password" />
+
+      <div class="actions-container">
+        <div class="checkbox-container">
+          <input type="checkbox" />
+          <label> Remember me</label>
+        </div>
+
+        <span> Forgot password?</span>
+      </div>
 
       <button>Log In</button>
     </form>
@@ -25,45 +34,45 @@ export default {
   margin-top: 12rem;
   display: flex;
   width: 100vw;
-  height: 100vh;
+  /* height: 100vh; */
+  margin-bottom: 4rem;
 
   align-items: center;
 }
 
 form {
-  height: 28rem;
-  width: 50rem;
-  background-color: var(--footer-bg-color);
-  border-radius: 0.75rem;
-  border: 2px solid rgba(255, 255, 255, 0.1);
-  padding: 3.5rem 2.5rem;
+  background-color: var(--login-bg-color);
+  padding: 2rem 3rem;
 }
 form * {
   font-family: "Gotham Pro";
-  color: var(--text-color);
+  color: var(--main-text-color);
   letter-spacing: 0.5px;
   outline: none;
   border: none;
 }
 form h3 {
+  font-family: "Gotham Pro Bold";
   font-size: 2rem;
-  font-weight: 500;
+  /* font-weight: 700; */
   line-height: 2rem;
-  text-align: center;
+  /* text-align: center; */
 }
 
-label {
+form > label {
   display: block;
   margin-top: 2rem;
   font-size: 1rem;
   font-weight: 500;
+  color: black;
 }
-input {
+form > input {
   display: block;
   height: 2.5rem;
   width: 100%;
   /* background-color: rgba(255, 255, 255, 0.07); */
-  border-radius: 0.25rem;
+  border-radius: 1.5rem;
+  border: 1px solid var(--main-text-color);
   padding: 0 1rem;
   margin-top: 0.5rem;
   font-size: 0.875rem;
@@ -72,15 +81,42 @@ input {
 ::placeholder {
   color: var(--text-color);
 }
-button {
-  margin-top: 3.5rem;
+
+.actions-container,
+.checkbox-container {
+  display: flex;
+  gap: 0.5rem;
+  width: fit-content;
+  align-items: center;
+}
+
+.checkbox-container > input {
+  height: 1rem;
+  width: 1rem;
+}
+
+.checkbox-container > label,
+.actions-container > span {
+    height: 0.875rem;
+    color: black;
+}
+
+.actions-container {
+  margin-top: 0.5rem;
   width: 100%;
-  background-color: var(--text-color);
+  justify-content: space-between;
+}
+
+button {
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  width: 100%;
+  background-color: var(--secondary-color);
   color: var(--footer-bg-color);
-  padding: 15px 0;
+  padding: 1rem 0;
   font-size: 1.25rem;
   font-weight: 600;
-  border-radius: 0.5rem;
+  border-radius: 2rem;
   cursor: pointer;
 }
 </style>
